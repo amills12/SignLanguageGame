e596bf49a96a6e7b124aa831cb9a235216cf1746
Tessinator <tolivia.christen@ufl.edu>
2021-03-08 12:36:09 -0500

    fixed error

--- Handsy/Assets/Scripts/SpawnerStatic.cs
+++ Handsy/Assets/Scripts/SpawnerStatic.cs
@@ -14,6 +14,7 @@ public class SpawnerStatic : MonoBehaviour
     public GameObject sprMask;
     //number of letters/numbers in the prefab array
     public int size = 36;
+    public bool timeDone = false;
 
     //use for determining which letter will be spawned from the arrary
     public int letIndex, score = 0; 
@@ -33,10 +34,11 @@ public class SpawnerStatic : MonoBehaviour
         /*if the current letter was signed correctly, it will be deactivated. 
         If deactivated, the game will destroy the current clone, reactivate the original object,
         generate a new random object from the array, then spawn it*/
-        if(!clonePrefab[letIndex].GetComponent<LetterSS>().isActivated)
+        if(timeDone && !clonePrefab[letIndex].GetComponent<LetterSS>().isActivated)
         {
             score++; //increment the score
             Destroy(clonePrefab[letIndex]);
+            timeDone = false;
             prefab[letIndex].GetComponent<LetterSS>().isActivated = true;
             letIndex = Random.Range(0,size);
             StartCoroutine(waitToStart(0.1f));
@@ -48,15 +50,17 @@ public class SpawnerStatic : MonoBehaviour
 
         //get the location of the desired letter spawn   
         location = spawnLocation.transform.position;
+
         //clone the desired prefab object to spawn at the desired location
         clonePrefab[letIndex] = Instantiate(prefab[letIndex], location, Quaternion.Euler(0,0,0)) as GameObject;
     }
 
     IEnumerator waitToStart(float n){
-
+        
         yield return new WaitForSeconds(n);
         animated.Play("AnPractice");
         Spawn();
+        timeDone = true;
         yield return new WaitForSeconds(0.5f);
         pencil.Play();
 
