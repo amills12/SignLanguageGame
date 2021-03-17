78f4ad4df6dfecc4b52f87bf4c7c7d129d84f201
amills12 <44168399+amills12@users.noreply.github.com>
2021-03-16 21:33:08 -0400

    Intro Menu No Longer Shows After Restart

--- Handsy/Assets/Scenes/Menu/PauseMenu.cs
+++ Handsy/Assets/Scenes/Menu/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour
 {    
     // Tells you if the game is paused, this is what you should import
     public static bool GameIsPaused = false;
+    public static bool GameIsRestarted = false;
     public GameObject pauseMenuUI;
     public GameObject gameUI;
     public GameObject introMenu;
@@ -15,16 +16,28 @@ public class PauseMenu : MonoBehaviour
     // On awake pause the game and turn on the intro screen
     void Awake()
     {
-        Time.timeScale = 0f;
-        GameIsPaused = true;
+        if(GameIsRestarted)
+        {
+            //If the game has ended and is being replayed, un-pause the game and disable intro menu
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+            GameIsRestarted = false;
+            introMenu.SetActive(false);
+        }
+        else
+        {
+            //If the game isn't a replay, pause the game and turn on the intro menu
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        // Run the pause menu off of escape button
-        if(Input.GetKeyDown(KeyCode.Escape))
+        // Run the pause menu off of escape button, This might need a logic rework depending on how many menus we have
+        if(Input.GetKeyDown(KeyCode.Escape) && !GameIsRestarted)
         {
             if(GameIsPaused && !introMenu.activeSelf)
             {
@@ -37,7 +50,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    public void Resume() 
+    public void Resume()
     {
         // Disable pause menu
         pauseMenuUI.SetActive(false);
@@ -52,8 +65,6 @@ public class PauseMenu : MonoBehaviour
     {
         // Stops the games by disabling time. (Pretty metal)
         Time.timeScale = 0f;
-
-        GameIsPaused= true;
     }
 
     void Pause()
@@ -77,12 +88,15 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMainMenu(string level)
     {
-        Resume();
+        Time.timeScale = 1f;
         SceneManager.LoadScene(level);
     }
 
 public void gameRestart()
 {
+    //Set game is restarted to true
+    GameIsRestarted = true;
+
     //Get current scene name
     string scene = SceneManager.GetActiveScene().name;
     
