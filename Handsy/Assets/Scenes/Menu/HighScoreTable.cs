48d942b7efb8d52725822b1bcb2666bea79e5842
amills12 <44168399+amills12@users.noreply.github.com>
2021-04-01 23:26:02 -0400

    Added Persistant Highscore

--- Handsy/Assets/Scenes/Menu/HighScoreTable.cs
+++ Handsy/Assets/Scenes/Menu/HighScoreTable.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+// This code was made with the help of https://www.youtube.com/watch?v=iAbaqGYdnyI&ab_channel=CodeMonkey
 public class HighScoreTable : MonoBehaviour
 {
     private Transform scoreContainer;
     private Transform scoreTemplate;
+    private List<Transform> scoreEntryTransformList;
 
     private void Awake(){
         scoreContainer = transform.Find("ScoreContainer");
@@ -14,26 +16,90 @@ public class HighScoreTable : MonoBehaviour
 
         scoreTemplate.gameObject.SetActive(false);
 
-        float templateHeight = 50f;
-        for(int i = 0; i < 8; i++)
-        {
-            Transform scoreTransform = Instantiate(scoreTemplate, scoreContainer);
-            RectTransform scoreRectTransform = scoreTransform.GetComponent<RectTransform>();
-            scoreRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
-            scoreTransform.gameObject.SetActive(true);
+        // scoreEntryList = new List<ScoreEntry>() {
+        //     new ScoreEntry{ score = 11, name = "AAA"},
+        //     new ScoreEntry{ score = 22, name = "BBB"},
+        //     new ScoreEntry{ score = 33, name = "CCC"},
+        //     new ScoreEntry{ score = 44, name = "DDD"},
+        //     new ScoreEntry{ score = 55, name = "EEE"},
+        //     new ScoreEntry{ score = 66, name = "FFF"},
+        //     new ScoreEntry{ score = 77, name = "GGG"},
+        // };
 
-            int rank = i + 1;
-            string rankString;
-            rankString = rank + "";
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
 
-            scoreTransform.Find("Rank").GetComponent<TextMeshProUGUI>().text = rankString;
-
-            string name = "AAA";
-
-            scoreTransform.Find("Name").GetComponent<TextMeshProUGUI>().text = name;
-
-            int score = Random.Range(0, 10000);
-            scoreTransform.Find("Score").GetComponent<TextMeshProUGUI>().text = score.ToString();
+        // Simple bubble sort on data
+        for (int i = 0; i < highscores.scoreEntryList.Count; i++) {
+            for (int j = i + 1; j < highscores.scoreEntryList.Count; j++) {
+                if (highscores.scoreEntryList[j].score > highscores.scoreEntryList[i].score) {
+                    // Preform a swap
+                    ScoreEntry temp = highscores.scoreEntryList[i];
+                    highscores.scoreEntryList[i] = highscores.scoreEntryList[j];
+                    highscores.scoreEntryList[j] = temp;
+                }
+            }
         }
+
+        scoreEntryTransformList = new List<Transform>();
+        foreach (ScoreEntry scoreEntry in highscores.scoreEntryList)
+        {
+            CreateScoreEntryTransform(scoreEntry, scoreContainer, scoreEntryTransformList);
+        }
+    }
+        
+
+    // This function adds a highscore to the table
+    private void CreateScoreEntryTransform(ScoreEntry scoreEntry, Transform container, List<Transform> transformList) {
+        float templateHeight = 50f;
+
+        Transform scoreTransform = Instantiate(scoreTemplate, container);
+        RectTransform scoreRectTransform = scoreTransform.GetComponent<RectTransform>();
+        scoreRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        scoreTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count + 1;
+        string rankString;
+        rankString = rank + "";
+
+        scoreTransform.Find("Rank").GetComponent<TextMeshProUGUI>().text = rankString;
+
+        // Grab the name and set the text
+        string name = scoreEntry.name;
+        scoreTransform.Find("Name").GetComponent<TextMeshProUGUI>().text = name;
+
+        // Grab score and set text
+        int score = scoreEntry.score;
+        scoreTransform.Find("Score").GetComponent<TextMeshProUGUI>().text = score.ToString();
+
+        transformList.Add(scoreTransform);
+    }
+
+    private void addScoreEntry(int score, string name){
+        // Create a new entry
+        ScoreEntry scoreEntry = new ScoreEntry{ score = score, name = name};
+
+        // Pull up the current highscore list
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        // Add the entry to the highscore list
+        highscores.scoreEntryList.Add(scoreEntry);
+
+        // Save updated highscore list
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+    }
+
+    private class Highscores {
+        public List<ScoreEntry> scoreEntryList;
+    }
+
+    // This is a structure for a single highscore entry
+    [System.Serializable]
+    private class ScoreEntry {
+        public int score;
+        public string name;
     }
 }
